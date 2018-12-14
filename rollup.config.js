229313d08601a1b63790f4c8efd11b3df8a04359
Jerry Martinez <jerry@jmweb.net>
2018-12-13 22:34:20 -0500

    Compile directly into BSI.modules.metaRefresh/parse/parse.js.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 export default {
     input: 'parseMetaRefresh.js',
     output: {
-        banner: '/* @requires /core/module/module.js */',
+        banner: [ '/* ', '@requires /core/module/module.js', `DO NOT EDIT! AUTO GENERATED ON ${ Date() } BY ${ __filename }`, '*/' ].join( "\n " ),
         file: 'dist/parseMetaRefresh.js',
         format: 'iife',
         strict: false
